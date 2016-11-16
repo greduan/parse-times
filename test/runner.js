f492c27377494237a91dde2415d871d7b8fed333
greduan <me@greduan.com>
2016-11-16 20:17:32 +0100

    Main func return Promise, basic (manual) test runner

---
+++ test/runner.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const path = require('path')
+const { resolve } = path
+
+const parser = require('..')
+
+const inFileNames = ['01.txt']
+const expectedOutFileNames = ['01.json']
+
+Promise
+  // parse all the files
+  .all(inFileNames.map((fileName) => {
+    return parser(resolve(__dirname, 'ins', fileName))
+  }))
+  .then(() => console.log('finished'))
